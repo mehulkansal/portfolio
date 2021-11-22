53b31381e0b9a530afa91c2f1c01e26ad4c1456f
mehul <24kansalmehul@gmail.com>
2021-11-22 15:37:56 +0530

    initial

--- review/admin.py
+++ review/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from review.models import review
+from review.models import review, counter
 
 # Register your models here.
 admin.site.register(review)
+admin.site.register(counter)
